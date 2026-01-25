e6d1c65f9be102a439ffa29159d9640574658ab4
dkochetkova <dkochetkova@ispras.ru>
2026-01-25 21:32:18 +0300

    updated get status handler

--- db/base.py
+++ db/base.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from env import FILE_DB
 
-engine = create_async_engine(f"sqlite+aiosqlite:///{FILE_DB}")
 
-Session = async_sessionmaker(engine)
+
+async_engine = create_async_engine(f"sqlite+aiosqlite:///{FILE_DB}", echo=False, pool_size=20, pool_recycle=295)
+Session = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
--- handlers/get_status_handler.py
+++ handlers/get_status_handler.py
@@ -1,14 +1,34 @@
+import datetime
+
 import aiosqlite
 from aiogram.types import Message
+from black.trans import defaultdict
 from sqlalchemy import select
 
 from db.base import Session
 from db.plan_orm import PlanORM
+from db.task_orm import TaskORM
 
 
 async def get_status_handler(message: Message) -> None:
     async with Session() as session:
         user_id = message.from_user.id
-        appropriate_request = select(PlanORM).where(PlanORM.user_id == user_id)
-        plans = await session.scalars(appropriate_request)
-    await message.answer(plans)
+        plan_query = select(PlanORM).where(PlanORM.user_id == user_id).order_by(PlanORM.created_at)
+        plans = (await session.scalars(plan_query)).all()
+        task_query = select(TaskORM).where(TaskORM.plan_id.in_([p.id for p in plans]) )
+        tasks = (await session.scalars(task_query)).all()
+
+
+    tasks_map = defaultdict(list)
+    for task in tasks:
+        tasks_map[task.plan_id].append(task)
+    answer = ""
+
+    for plan in plans:
+        answer += f"План: {datetime.datetime.fromtimestamp(plan.created_at)}"
+        answer += "\n".join(f"{task.status} {task.description}" for task in tasks_map[plan.id] )
+        answer += "\n"
+
+    if not answer:
+        answer = 'Планов нет'
+    await message.answer(answer)
